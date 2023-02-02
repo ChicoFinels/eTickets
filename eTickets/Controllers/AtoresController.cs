ab3548b16c725ccce5c27e58b6663a7e9af7a348
Finels <francisco.bonitos@gmail.com>
2023-02-02 19:53:34 +0000

    Adicionar AtoresController

---
+++ eTickets/Controllers/AtoresController.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class AtoresController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AtoresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var atores = await _context.Atores.ToListAsync();
+            return View(atores);
+        }
+    }
+}
