17ac52ff5af5e3dde77c6c8c0873d3c583058430
Finels <francisco.bonitos@gmail.com>
2023-02-02 15:45:42 +0000

    Adicionar modelo Diretor

---
+++ eTickets/Models/Diretor.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Diretor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        public string Biografia { get; set; }
+        public string FotoPerfilURL { get; set; }
+    }
+}
