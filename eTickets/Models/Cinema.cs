325b0da206afb1f9b482eff228de7998800e0051
Finels <francisco.bonitos@gmail.com>
2023-02-02 15:47:52 +0000

    Adicionar modelo Cinema

---
+++ eTickets/Models/Cinema.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Nome { get; set; }
+        public string LogoURL { get; set; }
+        public string Descricao { get; set; }
+    }
+}
