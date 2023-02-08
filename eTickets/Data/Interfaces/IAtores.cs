b45fb8b02d5a7c7330e2c06fefddb9ae779db8ce
Finels <francisco.bonitos@gmail.com>
2023-02-08 15:20:35 +0000

    Adicionar Interface IAtores

---
+++ eTickets/Data/Interfaces/IAtores.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Models;
+
+namespace eTickets.Data.Interfaces
+{
+    public interface IAtores
+    {
+        //obter todos os atores
+        IEnumerable<Ator> ObterTodos();
+        //obter ator pelo ID
+        Ator ObterPorId(int id);
+        //adicionar ator
+        void Adicionar(Ator ator);
+        //alterar um ator existente
+        Ator Atualizar(int id, Ator NovoAtor);
+        //apagar ator
+        void Apagar(int id);
+    }
+}
